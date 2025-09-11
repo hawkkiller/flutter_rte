0d8ae0594c1f1c38a7ee3b90a8806f3cde948a94
Michael Lazebny <miskadl09@gmail.com>
2025-09-11 20:19:47 +0200

    Initialize Flutter project structure with essential configuration files and a sample application

---
+++ rte/bin/rte.dart
@@ -0,0 +1,5 @@
+import 'package:rte/rte.dart' as rte;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rte.calculate()}!');
+}
---
+++ rte/test/rte_test.dart
@@ -0,0 +1,8 @@
+import 'package:rte/rte.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
