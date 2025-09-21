4e3e9a56d31a7a00bc831dc55dc9c1102a629425
Michael Lazebny <miskadl09@gmail.com>
2025-09-21 20:23:49 +0200

    feat: add document model
    This commit introduces the `Document` model.
    It includes selection and nodes properties.

---
+++ rte_core/lib/src/document.dart
@@ -0,0 +1,35 @@
+import 'package:collection/collection.dart';
+import 'package:rte_core/src/document_range.dart';
+import 'package:rte_core/src/node.dart';
+
+class Document {
+  const Document({
+    this.selection,
+    this.nodes = const [],
+  });
+
+  final DocumentRange? selection;
+  final List<BlockNode> nodes;
+
+  Document copyWith({
+    DocumentRange? selection,
+    List<BlockNode>? nodes,
+  }) {
+    return Document(
+      selection: selection ?? this.selection,
+      nodes: nodes ?? this.nodes,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Document &&
+        other.selection == selection &&
+        const DeepCollectionEquality().equals(other.nodes, nodes);
+  }
+
+  @override
+  int get hashCode => selection.hashCode ^ const DeepCollectionEquality().hash(nodes);
+}
